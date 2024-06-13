ca89d7276f714277d5250542c8bbba21b2a4dc93
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-06-13 23:28:16 +0530

    Frontend

---
+++ O_E_S Frontend/src/App.js
@@ -0,0 +1,32 @@
+import Home from "../Components/HomeComponent/Home";
+import StudentLogin from "./online-exam-system/Components/StudentComponent/StudentLogin/StudentLogin";
+import AdminLogin from "../Components/AdminComponent/AdminLogin/AdminLogin";
+import AdminDashboard from "../Components/AdminComponent/AdminDashboard/AdminDashboard";
+import StudentDashboard from "./online-exam-system/Components/StudentComponent/StudentDashboard/StudentDashboard";
+import {Route , BrowserRouter , Switch} from "react-router-dom";
+import StudentSignup from "./online-exam-system/Components/StudentComponent/StudentSignup/StudentSignup";
+
+ 
+
+
+    function App(){
+      return (
+        <>
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/StudentLogin" component={StudentLogin}></Route>
+                  <Route exact path="/StudentSignup" component={StudentSignup}></Route>
+                  <Route exact path="/AdminLogin" component={AdminLogin}></Route>
+                  <Route exact path="/AdminDashboard" component={AdminDashboard}></Route>
+                  <Route exact path="/StudentDashboard" component={StudentDashboard}></Route>
+              </Switch>
+          </BrowserRouter>
+        </>
+      );
+     
+    }
+
+      
+
+  export default App;
